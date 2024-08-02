05ab92d368e2fe43d4e9f2be6dea5e4e5c3b752a
Chris Moore <chrismoorelmt@gmail.com>
2024-08-02 00:57:08 -0500

    Added deployed app link to ReadMe

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {readFile, writeFile} = require('fs').promises;
 const path = require('path');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static("public"));
